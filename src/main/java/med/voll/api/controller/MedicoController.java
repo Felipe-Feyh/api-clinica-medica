ca16dd853b1a30e6058a4d9e0649a76a56331497
felipe feyh <felipefeyh2@gmail.com>
2024-07-21 10:23:36 -0300

    Implementar paginacao na consulta de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,15 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -25,9 +25,15 @@ public class MedicoController {
        repository.save(new Medico(dados));
     }
 
+    /* o pageable implementa a paginação
+    exemplos de parametros de paginação
+    size=1&page=1&sort=nome&desc
+    */
+
+    // @PageableDefault(size = 10, sort = "nome" é default, se for passado um valor diferente do default, o valor passado será o valor default)
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
     @GetMapping
