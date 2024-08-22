c33ca93b8f21bdb48efa98915ba1e5ccf10293d4
felipe feyh <felipefeyh2@gmail.com>
2024-08-22 11:41:51 -0300

    feat: Implementar regras de negócio

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorHorarioFuncionamentoClinica implements ValidadorAgendamentoDeConsulta {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(java.time.DayOfWeek.SUNDAY);
+        var antesDaAbertura = dataConsulta.getHour() < 7;
+        var depoisDaFechamento = dataConsulta.getHour() > 18;
+        if (domingo || antesDaAbertura || depoisDaFechamento) {
+            throw new ValidacaoException("Consulta fora do horário de funcionamento");
+        }
+    }
+}
